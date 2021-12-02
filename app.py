59b8e8c8f66b8095719202d194a5a4e35a28d121
Liam <pwnface4@gmail.com>
2021-12-02 11:24:04 -0800

    changed sqllite test code to mysqldb

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask
 from flask import render_template
 import sqlite3
-# import mysql.connector
+import mysql.connector
 
-# mydb = mysql.connector.connect(
-#   host="localhost",
-#   user="yourusername",
-#   password="yourpassword",
-#   database="mydatabase"
-# )
+mydb = mysql.connector.connect(
+  host="database-1.cxli1xpe8prx.us-east-1.rds.amazonaws.com",
+  user="web_app",
+  password="Password!",
+  database="restaurant",
+  auth_plugin="mysql_native_password"
+)
 
-# print(mydb)
+print(mydb)
 
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+# def get_db_connection():
+#     conn = sqlite3.connect('database.db')
+#     conn.row_factory = sqlite3.Row
+#     return conn
 
 
 
@@ -32,8 +33,9 @@ def index():
 
 @app.route('/menu')
 def menu():
-    conn = get_db_connection()
-    menu = conn.execute('SELECT * FROM menu').fetchall()
+    conn = mydb.cursor()
+    conn.execute('SELECT * FROM menu')
+    menu = conn.fetchall()
     print(menu)
     conn.close()
     return render_template('./menu.html', menu=menu)
